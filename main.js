a6684f64e4e839d816c9715d408bd70a56c917c8
EricKugel <erickugel713@gmail.com>
2022-04-17 15:50:55 -0500

    Basic move selection and stuff

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var table;
+var selectedSquare = "";
 
 window.onload = function() {
     table = document.getElementById("table");
@@ -8,13 +9,34 @@ window.onload = function() {
         var rowElement = document.createElement("tr");
         for (var col = 0; col < 8; col++) {
             var colElement = document.createElement("td");
-            colElement.style.backgroundColor = (row + col) % 2 == 1 ? "#7d945d" : "#eeeed5"; 
+            colElement.style.backgroundColor = (row + col) % 2 == 1 ? "#7d945d" : "#eeeed5";
+            colElement.id = "" + "abcdefgh".charAt(col) + (8 - row);
+            colElement.onclick = function() {
+                squareClicked(this.id);
+            } 
             rowElement.appendChild(colElement);
         }
         tbody.appendChild(rowElement);
     }
 
     gameLoop();
+}
+
+function squareClicked(square) {
+    if (selectedSquare == "" && getSquare(square).children.length > 0) {
+        selectedSquare = square;
+        getSquare(square).className = "selected";
+    } else {
+        makeMove(selectedSquare, square);
+        getSquare(selectedSquare).className = "";
+        selectedSquare = "";
+    }
+}
+
+function getSquare(square) {
+    var row = 8 - parseInt("" + square.charAt(1));
+    var col = "abcdefgh".indexOf("" + square.charAt(0));
+    return table.children[0].children[row].children[col];
 }
 
 function gameLoop() {
@@ -28,6 +50,11 @@ function gameLoop() {
             }
         }
     }
+}
+
+function makeMove(from, to) {
+    getSquare(to).innerHTML = getSquare(from).innerHTML;
+    getSquare(from).innerHTML = "";
 }
 
 function getBoard() {

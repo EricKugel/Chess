16758d7f228fb737fc25a81babae832b5f7315c7
EricKugel <erickugel713@gmail.com>
2022-04-17 15:16:18 -0500

    Basic stuff is working

--- main.js
+++ main.js
@@ -8,10 +8,50 @@ window.onload = function() {
         var rowElement = document.createElement("tr");
         for (var col = 0; col < 8; col++) {
             var colElement = document.createElement("td");
-            colElement.innerText = row + ", " + col;
-            colElement.style.backgroundColor = (row + col) % 2 == 1 ? "#000000" : "#FFFFFF"; 
+            colElement.style.backgroundColor = (row + col) % 2 == 1 ? "#7d945d" : "#eeeed5"; 
             rowElement.appendChild(colElement);
         }
         tbody.appendChild(rowElement);
     }
+
+    gameLoop();
+}
+
+function gameLoop() {
+    var board = getBoard();
+    for (var row = 0; row < 8; row++) {
+        for (var col = 0; col < 8; col++) {
+            var piece = board[row][col];
+            if (piece != "") {
+                var cell = table.children[0].children[row].children[col];
+                cell.innerHTML = "<img src = \"" + "lib/" + piece + ".png\"/>";
+            }
+        }
+    }
+}
+
+function getBoard() {
+    var fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    var board = [];
+
+    var fenRows = fen.substring(0, fen.indexOf(" ")).split("/");
+    for (var row = 0; row < fenRows.length; row++) {
+        var fenRow = fenRows[row];
+        var boardRow = [];    
+        for (var letterIndex = 0; letterIndex < fenRow.length; letterIndex++) {
+            var letter = "" + fenRow.charAt(letterIndex);
+            if ("rnbqkp".includes(letter)) {
+                boardRow.push(letter + "d");
+            } else if ("rnbqkp".includes(letter.toLowerCase())) {
+                boardRow.push(letter.toLowerCase() + "l")
+            } else if ("12345678".includes(letter)) {
+                for (var i = 0; i < parseInt(letter); i++) {
+                    boardRow.push("");
+                }
+            }
+        }
+        board.push(boardRow);
+    }
+
+    return board;
 }
